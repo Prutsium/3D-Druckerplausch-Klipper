98b8a9d483cac41a0402682e9e83fc6c4ac25056
Dennis <dennis.henke@mailbox.org>
2022-02-18 16:49:32 +0100

    MMU: Simplified syncing extruders, extruding, retracting and so on

--- Klipper_cfg/Prusa_Universal_Config_Revised/MMU/Python/mmu2s.py
+++ Klipper_cfg/Prusa_Universal_Config_Revised/MMU/Python/mmu2s.py
@@ -16,7 +16,6 @@ class Mmu2s:
         self.common_variables = self.printer.lookup_object("save_variables").allVariables
         self.mmu_variables = self.printer.lookup_object("gcode_macro _VAR_MMU2S").variables
         self.printer.register_event_handler("klippy:connect", self.handle_connect)
-        self.gcode = self.printer.lookup_object('gcode')
 
         # Sensors
         self.ir_sensor = self.printer.lookup_object("filament_switch_sensor ir_sensor")
@@ -25,6 +24,7 @@ class Mmu2s:
         # Initial values
         self.homed = False
         self.locked = False
+        self.extruder_steppers_synced = False
         self.idler_position = -1
         self.selector_position = -1
         self.led_enabled = bool(self.mmu_variables["enable_led"])
@@ -33,7 +33,11 @@ class Mmu2s:
         self.selector_stepper = None
         self.idler_stepper = None
 
-        # GCODE commands
+        # Gcode
+        self.gcode = self.printer.lookup_object('gcode')
+        self.register_gcode_commands()
+
+    def register_gcode_commands(self):
         self.gcode.register_command('MMU_LOAD_FILAMENT_TO_FINDA',
                     self.cmd_MMU_LOAD_FILAMENT_TO_FINDA,
                     desc=self.cmd_MMU_LOAD_FILAMENT_TO_FINDA_help)
@@ -115,34 +119,56 @@ class Mmu2s:
         if self.idler_stepper is None:
             raise self.config.error("Manual_stepper idler_stepper must be specified")
 
+    def sync_extruder_steppers(self):
+        if not self.extruder_steppers_synced:
+            self.gcode.run_script_from_command("SYNC_STEPPER_TO_EXTRUDER STEPPER=gear_stepper EXTRUDER=extruder")
+            self.gcode.run_script_from_command("G92 E0")
+            self.extruder_steppers_synced = True
+
+    def unsync_extruder_steppers(self):
+        if self.extruder_steppers_synced:
+            self.gcode.run_script_from_command("SYNC_STEPPER_TO_EXTRUDER STEPPER=gear_stepper EXTRUDER=")
+            self.gcode.run_script_from_command("G92 E0")
+            self.extruder_steppers_synced = False
+
+    def ir_sensor_state(self):
+        return bool(self.ir_sensor.runout_helper.filament_present)
+
+    def finda_sensor_state(self):
+        return bool(self.finda_sensor.runout_helper.filament_present)
+
     cmd_MMU_LOAD_FILAMENT_TO_FINDA_help = "Load filament until Finda registers it"
     def cmd_MMU_LOAD_FILAMENT_TO_FINDA(self, gcmd):
         stepsize = 10
         loaded = False
-        sensor_state = bool(self.finda_sensor.runout_helper.filament_present)
 
-        if sensor_state:
+        if self.finda_sensor_state():
             gcmd.respond_info("Filament already in finda")
         else:
-            self.gcode.run_script_from_command("SYNC_STEPPER_TO_EXTRUDER STEPPER=gear_stepper EXTRUDER=extruder")
             self.gcode.run_script_from_command("G92 E0")
             for i in range (0, 200, stepsize):
-                if not bool(self.finda_sensor.runout_helper.filament_present):
+                if not self.finda_sensor_state():
                     gcmd.respond_info("Loaded %s mm of filament to finda." % (i))
-                    self.gcode.run_script_from_command("G1 E%s F%s" % (i, 40*60))
-                    self.gcode.run_script_from_command("M400")
+                    self.extrude(stepsize, 40*60)
                 else:
-                    self.gcode.run_script_from_command("G1 E%s F%s" % (i, 40*60))
-                    self.gcode.run_script_from_command("M400")
+                    self.extrude(stepsize, 40*60)
                     loaded = True
                     break
             self.gcode.run_script_from_command("G92 E0")
-            sleep(2)
-            self.gcode.run_script_from_command("SYNC_STEPPER_TO_EXTRUDER STEPPER=gear_stepper EXTRUDER=")	
             if loaded:
                 gcmd.respond_info("Filament loaded successfully to finda")  
             else:
                 gcmd.respond_info("FILAMENT NOT IN FINDA!!!")  
+
+    def extrude(self, length, feedrate):
+        if length and feedrate:
+            self.gcode.run_script_from_command("G92 E0")
+            self.gcode.run_script_from_command("G1 E%s F%s" % (length, feedrate))
+            self.gcode.run_script_from_command("M400")
+
+    def retract(self, length, feedrate):
+        if length and feedrate:
+            self.extrude(-length, feedrate)
 
     cmd_MMU_LOAD_FILAMENT_TO_EXTRUDER_help = "Load filament from Finda to Extruder"
     def cmd_MMU_LOAD_FILAMENT_TO_EXTRUDER(self, gcmd):
@@ -153,63 +179,48 @@ class Mmu2s:
         final_load_endvalue = None
         bowden_length = self.common_variables.get("mmu_bowden_length", -1)
 
-        finda_state = bool(self.finda_sensor.runout_helper.filament_present)
-
-        if not finda_state:
+        if not self.finda_sensor_state():
             self.cmd_MMU_LOAD_FILAMENT_TO_FINDA(gcmd)
-            finda_state = bool(self.finda_sensor.runout_helper.filament_present)
 
-        if not finda_state:
-            gcmd.respond_info("Filament not in Finda!")
-            return
+            if not self.finda_sensor_state():
+                raise gcmd.error("Filament not in Finda!")
 
-        sensor_state = bool(self.ir_sensor.runout_helper.filament_present)
-        if sensor_state:
+        if self.ir_sensor_state():
             gcmd.respond_info("Filament already in Extruder!")
             return
         else:
-            self.gcode.run_script_from_command("SYNC_STEPPER_TO_EXTRUDER STEPPER=gear_stepper EXTRUDER=extruder")
             self.gcode.run_script_from_command("G92 E0")
             
             if bowden_length == -1:    
                 for i in range (0, 700, stepsize):
-                    if not bool(self.ir_sensor.runout_helper.filament_present):
+                    if not self.ir_sensor_state():
                         gcmd.respond_info("Approx. Loading: %s mm of filament to Extruder." % (i))
-                        self.gcode.run_script_from_command("G1 E%s F%s" % (i, 2400))
-                        self.gcode.run_script_from_command("M400")
+                        self.extrude(stepsize, 2400)
                     else:
                         loaded = True
                         final_load_startvalue = i - stepsize - 5
-                        self.gcode.run_script_from_command("G92 E0")
-                        self.gcode.run_script_from_command("G1 E-%s F%s" % (stepsize+5, 2400))
-                        self.gcode.run_script_from_command("M400")
+                        self.retract(stepsize+5, 2400)
                         break
                 if not loaded:
-                    gcmd.respond_info("Extruded %s mm Filament, but nothing in Extruder!!" % (i))  
+                    raise gcmd.error("Extruded %s mm Filament, but nothing in Extruder!!" % (i))  
                     return
             else:
                 final_load_startvalue = bowden_length*0.85
                 gcmd.respond_info("Fast Loading with saved variable: %s mm of filament to Extruder." % (final_load_startvalue))
-                self.gcode.run_script_from_command("G1 E%s F%s" % (final_load_startvalue, 3000))
-                self.gcode.run_script_from_command("M400")
+                self.extrude(final_load_startvalue, 3000)
 
             sleep(1)
-            self.gcode.run_script_from_command("G92 E0")
             for j in range (0, stepsize * 10, smallsteps):
-                if not bool(self.ir_sensor.runout_helper.filament_present):
+                if not self.ir_sensor_state():
                     gcmd.respond_info("Final Loading: %s mm of filament to Extruder." % (final_load_startvalue + j))
-                    self.gcode.run_script_from_command("G1 E%s F%s" % (j, 600))
-                    self.gcode.run_script_from_command("M400")
+                    self.extrude(smallsteps, 600)
                 else:
-                    self.gcode.run_script_from_command("G1 E%s F%s" % (j, 600))
-                    self.gcode.run_script_from_command("M400")
+                    self.extrude(smallsteps, 600)
                     loaded = True
                     final_load_endvalue = final_load_startvalue + j
                     break
 
             self.gcode.run_script_from_command("G92 E0")
-            sleep(2)
-            self.gcode.run_script_from_command("SYNC_STEPPER_TO_EXTRUDER STEPPER=gear_stepper EXTRUDER=")	
             if loaded:
                 gcmd.respond_info("Filament loaded successfully to Extruder (%s mm)" % (final_load_endvalue))
                 if bowden_length == -1:
@@ -226,34 +237,22 @@ class Mmu2s:
 
         if paused == 0:
             if tool != -1:
-                sensor_state = bool(self.ir_sensor.runout_helper.filament_present)
-
-                if sensor_state:
+                if self.ir_sensor_state():
                     gcmd.respond_info("Unloading filament from extruder to FINDA ...")
-                    self.gcode.run_script_from_command("SYNC_STEPPER_TO_EXTRUDER STEPPER=gear_stepper EXTRUDER=extruder")
-                    self.gcode.run_script_from_command("G92 E0")
-                    self.gcode.run_script_from_command("G1 E-%s F%s" % (bowden_length*0.85, 3000))
-                    self.gcode.run_script_from_command("M400")
+                    self.retract(bowden_length*0.85, 3000)
                     
-                finda = self.printer.lookup_object("filament_switch_sensor finda")
-                finda_state = bool(finda.runout_helper.filament_present)
-
-                if finda_state:
+                if self.finda_sensor_state():
                     gcmd.respond_info("Unloading filament from FINDA ...")
                     self.gcode.run_script_from_command("G92 E0")
                     for i in range (0, 600, stepsize):
-                        if bool(self.finda_sensor.runout_helper.filament_present):
+                        if self.finda_sensor_state():
                             gcmd.respond_info("Unloaded %s mm of filament to finda." % (i))
-                            self.gcode.run_script_from_command("G1 E-%s F%s" % (i, 40*60))
-                            self.gcode.run_script_from_command("M400")
+                            self.retract(i, 40*60)
                         else:
-                            self.gcode.run_script_from_command("G1 E-%s F%s" % (i+5, 40*60))
-                            self.gcode.run_script_from_command("M400")
+                            self.retract(i+5, 40*60)
                             break
                 else:
-                    self.gcode.run_script_from_command("G92 E0")
-                    self.gcode.run_script_from_command("G1 E-%s F%s" % (25, 1000))
-                self.gcode.run_script_from_command("SYNC_STEPPER_TO_EXTRUDER STEPPER=gear_stepper EXTRUDER=")
+                    self.retract(25, 1000)
                 gcmd.respond_info("Unloading done from FINDA to extruder")
             else:
                 gcmd.respond_info("Cannot unload from extruder to FINDA, tool not selected !!")  
@@ -279,6 +278,12 @@ class Mmu2s:
         else:
             gcmd.respond_info("No change needed. Tool %s already active." % (tool))
 
+#
+#Extrude Filament
+    #Wait_for_finished
+#Retract Filament
+    #Wait_for_finished
+
     cmd_MMU_LT_help = "Load filament from MMU2S to nozzle"
     def cmd_MMU_LT(self, gcmd):
         try:
@@ -290,7 +295,7 @@ class Mmu2s:
             gcmd.respond_info("LT %s" % (tool))
             self.cmd_MMU_SELECT_TOOL(gcmd)
             self.cmd_MMU_LOAD_FILAMENT_TO_EXTRUDER(gcmd)
-            self.gcode.run_script_from_command("MMU_LOAD_FILAMENT_INTO_NOZZLE")
+            self.load_filament_into_nozzle(gcmd)
         else:
             raise gcmd.error("NO LT MADE, MMU is locked")
 
@@ -306,6 +311,27 @@ class Mmu2s:
                 raise gcmd.error("NO UT MADE, no tool activate")
         else:
             raise gcmd.error("NO UT MADE, MMU is locked")
+
+    def load_filament_into_nozzle(self, gcmd):
+        hotend_length = self.common_variables.get("mmu_hotend_length", -1)
+        if hotend_length == -1:
+            gcmd.respond_info("Hotend length for loading check not configured. Using and saving: 50mm")
+            hotend_length = 50
+            self.gcode.run_script_from_command("SAVE_VARIABLE VARIABLE=mmu_hotend_length VALUE=%s" % (hotend_length))
+
+        if self.ir_sensor_state():
+            self.gcode.run_script_from_command("G92 E0")
+
+            self.extrude(hotend_length, 10*60)
+            self.retract(hotend_length-2, 20*60)
+
+            if self.ir_sensor_state():
+                self.extrude(hotend_length, 10*60)
+                gcmd.respond_info("Check load filament to nozzle successful")
+            else:
+                raise gcmd.error("Hotend possibly clogged!!")
+
+            self.gcode.run_script_from_command("G92 E0")
 
     cmd_MMU_SELECT_TOOL_help = "Select a tool. move the idler and then move the color selector (if needed)"
     def cmd_MMU_SELECT_TOOL(self, gcmd):
@@ -351,6 +377,10 @@ class Mmu2s:
             if self.homed:
                 self.gcode.run_script_from_command("MANUAL_STEPPER STEPPER=idler_stepper MOVE=%s" % (positions[tool]))
                 self.idler_position = tool
+                if tool == -1:
+                    self.unsync_extruder_steppers()
+                else:
+                    self.sync_extruder_steppers()
             else:
                 raise gcmd.error("Did not move idler, MMU is not homed.")
         else:
@@ -383,7 +413,7 @@ class Mmu2s:
         self.gcode.run_script_from_command("G90")
         self.gcode.run_script_from_command("G1 X%s Y%s F3000" % (self.mmu_variables["pause_x"], self.mmu_variables["pause_y"]))
         self.cmd_MMU_UT(gcmd)
-        if not bool(self.finda_sensor.runout_helper.filament_present):
+        if not self.finda_sensor_state():
             gcmd.respond_info("M702 ok ...")
         else:
             raise gcmd.error("M702 Error !!!")
@@ -401,7 +431,7 @@ class Mmu2s:
         self.ir_sensor.sensor_enabled = False
         gcmd.respond_info("Homing MMU ...")        
         if not self.locked:
-            if bool(self.ir_sensor.runout_helper.filament_present):
+            if self.ir_sensor_state():
                 gcmd.respond_info("Filament in extruder, trying to eject it ..")
                 self.gcode.run_script_from_command("MMU_UNLOAD_FILAMENT_IN_EXTRUDER")
             self.gcode.run_script_from_command("_MMU_IS_FILAMENT_STUCK_IN_EXTRUDER")
@@ -444,7 +474,7 @@ class Mmu2s:
     cmd_MMU_RESUME_help = "Macro to CORRECTLY RESUME with MMU check."
     def cmd_MMU_RESUME(self, gcmd):
         if not self.locked:
-            if bool(self.ir_sensor.runout_helper.filament_present):
+            if self.ir_sensor_state():
                 gcmd.respond_info("Resuming print")
                 self.gcode.run_script_from_command("RESUME")
             else:
@@ -470,12 +500,8 @@ class Mmu2s:
                 self.cmd_MMU_MOVE_SELECTOR({"VALUE":tool+1})
             else:
                 self.cmd_MMU_MOVE_SELECTOR({"VALUE":-1})
-        self.gcode.run_script_from_command("SYNC_STEPPER_TO_EXTRUDER STEPPER=gear_stepper EXTRUDER=extruder")
-        self.gcode.run_script_from_command("G91")
+        self.extrude(8, 1200)
         self.gcode.run_script_from_command("G92 E0")
-        self.gcode.run_script_from_command("G1 E8 F1200")
-        self.gcode.run_script_from_command("G92 E0")
-        self.gcode.run_script_from_command("SYNC_STEPPER_TO_EXTRUDER STEPPER=gear_stepper EXTRUDER=")
         self.gcode.run_script_from_command("SET_TMC_CURRENT STEPPER=selector_stepper CURRENT=1.00 HOLDCURRENT=0.400")
         self.gcode.run_script_from_command("INIT_TMC STEPPER=selector_stepper")
         self.cmd_MMU_MOVE_SELECTOR({"VALUE": 0})
@@ -483,10 +509,6 @@ class Mmu2s:
         self.gcode.run_script_from_command("INIT_TMC STEPPER=selector_stepper")
         self.gcode.run_script_from_command("MMU_HOME_SELECTOR")
         self.cmd_MMU_MOVE_SELECTOR(gcmd)
-        self.gcode.run_script_from_command("")
-        self.gcode.run_script_from_command("")
-        self.gcode.run_script_from_command("")
-
 
 def load_config(config):
     return Mmu2s(config)
